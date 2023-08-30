f4e4ce0185412e27ade0e1cc7d2fef1f44dd53e9
JungSuHwan23 <short9666@gmail.com>
2023-08-31 08:50:23 +0900

    .

--- codeup.py
+++ codeup.py
@@ -106,3 +106,29 @@ result = int(a)
 for i in range(0, n-1) :
     result = result*m+d 
 print(result)
+
+#6091
+a, b, c = map(int, input().split())
+d = 1
+while d%a!=0 or d%b!=0 or d%c!=0 :
+    d += 1
+print(d)
+
+#6092
+n = int(input())
+a = input().split()
+
+for i in range(n) :
+    a[i] = int(a[i])
+
+d = []
+for i in range(24) :
+    d.append(0)
+
+for i in range(n) :
+    d[a[i]] += 1
+
+for i in range(1, 24) :
+    print(d[i], end=" ")
+
+#6093
