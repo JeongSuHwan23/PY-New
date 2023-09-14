7c7f7421fb8f36d25f7fbeafca0b6e22aa4b4838
JungSuHwan23 <short9666@gmail.com>
2023-09-14 09:27:18 +0900

    09-14 class

---
+++ class/09-14.py
@@ -0,0 +1,12 @@
+a = [[0]*19 for _ in range(19)]
+
+n = int(input())
+
+for i in range (n) :
+    x, y = input().split()
+    a[int(x)-1][int(y)-1] = 1
+
+for i in range (19) :
+    for j in range (19) :
+        print(a[i][j], end=' ')
+    print()
