9ae48cd3d4da9f240add172dacc732cbb5f05091
JungSuHwan23 <short9666@gmail.com>
2023-11-22 20:03:35 +0900

    11-22

--- School/Lee/Class/Report/stack adt 8.py
+++ School/Lee/Class/Report/stack adt 8.py
@@ -13,6 +13,7 @@ class Stack:
   def isFull(self):
     return self.top == self.size-1
 
+  #값을 삽입하는 함수
   def push(self, element):
     if self.isFull():
       print("Full")
@@ -21,23 +22,71 @@ class Stack:
       self.top += 1
       self.stack.insert(self.top, element)
 
+
+  #가장 위에 있는 값을 출력하고 제거하는 함수
   def pop(self):
     if self.isEmpty():
       print("Empty")
       pass
     else:
+      print("pop :", self.stack[self.top])
       self.top -= 1
-      self.stack.pop()
 
+  #가장 위에 있는 값만 출력하는 함수
   def peek(self):
     if self.isEmpty():
-      print(self.stack[self.top])
-    else:
+      print("Empty")
       pass
+    else:
+      print("peek :", self.stack[self.top])
 
-  #def print(self):
+  #스택 전체를 출력하는 함수
+  def printAll(self):
+    for i in range(self.top+1):
+      print(self.stack[i], end="->")
+    print()
 
-  #def stackSize(self):
+  #모든 값을 출력하는 함수
+  def valueSum(self):
+    sum = 0
+    for i in range (self.top+1):
+      sum += self.stack[i]
+    print("Sum result is", sum)
 
-  #def clear:
+  #스택 초기화 하는 함수
+  def clear(self):
+    s.top = -1
+    print("Clear")
 
+if __name__ == "__main__":
+  s = Stack()
+  while True:
+    print("1 : push | 2 : pop | 3 : peek | 4 : sum | 5 : print | 6 : clear | 0 : exit")
+    n = int(input())
+
+    if n == 1:
+      print("values :", end=" ")
+      value = int(input())
+      s.push(value)
+      continue
+    elif n == 2:
+      s.pop()
+      continue
+    elif n == 3:
+      s.peek()
+      continue
+    elif n == 4:
+      s.valueSum()
+      continue
+    elif n == 5:
+      s.printAll()
+      continue
+    elif n == 6:
+      s.clear()
+      continue
+    elif n == 0:
+      print("BYE")
+      break
+    else:
+      print("err")
+      continue
