abc9f8a720a37c447ca5d5b0b6b7cde62634e446
jeongsuhwan <short9666@gmail.com>
2024-03-05 08:00:13 +0900

    3-5

--- Baekjoon/Codetest_Basic/Brute_force.py
+++ Baekjoon/Codetest_Basic/Brute_force.py
@@ -1,14 +1,21 @@
+#일곱 난쟁이
 h = []
 for i in range(9):
-  temp = input()
-  h.append(int(temp))
+  h.append(int(input()))
+
 h.sort()
+
 h_sum = sum(h)
+
 for i in range(len(h)):
-	for j in range(i+1, len(h)):
-				if h_sum - h[i] - h[j] == 100:
-					for k in range(9):
-						if k == j or k == i:
-							pass
-						else:
-							print(h[k])
+  for j in range(i+1, len(h)):
+    if h_sum - h[i] - h[j] == 100:
+      for k in range(len(h)):
+        if k == j or k == i:
+          pass
+        else:
+          print(h[k])
+      exit()
+
+#사탕 게임
+
