ae0f1322fcafce9ec9c71abbb29932542581be0b
jeongsuhwan <short9666@gmail.com>
2024-03-05 20:20:50 +0900

    3-5

--- Baekjoon/Codetest_Basic/Brute_force.py
+++ Baekjoon/Codetest_Basic/Brute_force.py
@@ -17,5 +17,50 @@ for i in range(len(h)):
           print(h[k])
       exit()
 
-#사탕 게임
+#n과 m (1)
+n,m = list(map(int,input().split()))
+s = []
+def dfs():
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(1,n+1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+dfs()
+
+#n과 m (2)
+def dfs(start):
+  if len(s) == m:
+    print(' '.join(map(str, s)))
+  
+  for i in range(start, n+1):
+    if i not in s:
+      s.append(i)
+      dfs(i+1)
+      s.pop()
+      
+n, m = list(map(int,input().split()))
+s = []
+dfs(1)
+
+#n과 m (3)
+def dfs():
+  if len(s) == m:
+    print(' '.join(map(str, s)))
+    return
+  
+  for i in range(1, n+1):
+    s.append(i)
+    dfs()
+    s.pop()
+      
+n, m = map(int,input().split())
+s = []
+dfs()
+
+
 
